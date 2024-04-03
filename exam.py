bd551cda62da40e052cb4198603df4ea3a8dcfdb
Seongmin PARK <seongmin.park@lginnotek.com>
2024-04-03 13:27:50 +0900

    Second

--- exam.py
+++ exam.py
@@ -1,7 +1,9 @@
-import requests
+# import requests
+#
+# url = 'http://<3.141.26.126>:5000/api'  # EC2 인스턴스의 IP 주소로 변경하세요.
+# data = {'key1': 'value1', 'key2': 'value2'}  # 전송할 데이터
+#
+# response = requests.post(url, json=data)
+# print(response.json())
 
-url = 'http://<3.141.26.126>:5000/api'  # EC2 인스턴스의 IP 주소로 변경하세요.
-data = {'key1': 'value1', 'key2': 'value2'}  # 전송할 데이터
-
-response = requests.post(url, json=data)
-print(response.json())
+print("hello AWS")
