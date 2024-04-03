5c37021c807a2f614ceb5ee73d22b4506c8801b2
Seongmin PARK <seongmin.park@lginnotek.com>
2024-04-03 15:32:39 +0900

    no message

--- exam.py
+++ exam.py
@@ -6,4 +6,17 @@
 # response = requests.post(url, json=data)
 # print(response.json())
 
-print("hello AWS")
+# receive_data.py
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/receive_data", methods=["POST"])
+def receive_data():
+    data = request.get_json()
+    message = data.get("message")
+    print("Received message:", message)
+    return "Data received successfully!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)  # 외부에서 접근 가능한 포트로 설정
